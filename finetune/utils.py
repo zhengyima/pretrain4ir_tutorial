bba2b5f092c7ca2af59c5d3b68ff3ec73f332a5d
ZYMa <317958662@qq.com>
2021-09-11 03:10:47 +0800

    Create utils.py

---
+++ finetune/utils.py
@@ -0,0 +1,13 @@
+    
+import random, os, torch
+import numpy as np
+def set_seed(seed=0):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
