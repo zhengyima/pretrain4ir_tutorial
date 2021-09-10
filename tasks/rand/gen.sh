c7737ed4b91957c4b1bffa565a1d11aaa4e03bc5
ZYMa <317958662@qq.com>
2021-09-11 03:17:13 +0800

    Update gen.sh

--- tasks/rand/gen.sh
+++ tasks/rand/gen.sh
@@ -1,4 +1,4 @@
 python gendata.py --msmarco_docs_path /home/dou/replearn/data/msmarco/msmarco-docs.tsv \
     --bert_model /home/dou/replearn/transformers_models/bert \
-    --output_file /home/dou/replearn/pretrain4ir_tutorial/datas/rand/data.json \
+    --output_file ../../datas/rand/data.json \
     --docs_limit 100
